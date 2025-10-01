e449da02720369524c3aed7c0a0178ab4ecfd7fd
Allengl <guolong613@gmail.com>
2025-10-01 22:39:46 +0800

    学习 rust 基础

---
+++ my_project/src/lib.rs
@@ -0,0 +1,72 @@
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+//
+// pub fn eat_at_restaurant() {
+//     crate::front_of_house::hosting::add_to_waitlist();
+//
+//     front_of_house::hosting::add_to_waitlist();
+// }```rust
+// fn serve_order() {}
+//
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::serve_order();
+//     }
+//
+//     fn cook_order() {}
+// }
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+//
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+//
+// pub fn eat_at_restaurant() {
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+//     meal.seasonal_fruit = String::from("blueberries");
+// }
+
+// mod back_of_house {
+//     pub enum Appetizer {
+//         Soup,
+//         Salad,
+//     }
+// }
+//
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+//
+// use crate::front_of_house::hosting;
+//
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+//     hosting::add_to_waitlist();
+//     hosting::add_to_waitlist();
+//
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn f1() -> Result {}
+
+fn f2() -> IoResult {}
+
+fn main() {}
---
+++ my_project/src/main.rs
@@ -0,0 +1,6 @@
+// use std::io;
+// use std::io::Write;
+use std::collections::*;
+use std::io::{self, Write};
+fn main() {}
+
